9e4fee6676af38461deca28b3e685af39023aa3d
Deep patel <deepp2618@gmail.com>
2020-08-14 21:02:06 -0400

    added

--- AuthorFactory.java
+++ AuthorFactory.java
@@ -1,6 +1,23 @@
 package Book;
 
 public class AuthorFactory {
+	
+	//making the singleton 
+	AuthorFactory obj;
+	
+	private AuthorFactory() {
+		
+		public AuthorFactory getInstance() {
+			if(obj == null ) {
+				obj = new AuthorFactory();
+				
+			}
+			return obj;
+		}
+		
+	}
+	
+	//This si the Implemenatation of the Factroy design pattern Deep patel
  Author a = new Author("Brad Thor");
 	public Author makingAuthor(String name) {
 		 if(name.equals("Brad Thor")) {
