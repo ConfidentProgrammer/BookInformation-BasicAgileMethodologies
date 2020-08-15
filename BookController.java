e9a5595d34c01e68e61322b2fe046a1ef4b03703
Deep patel <deepp2618@gmail.com>
2020-08-14 20:13:13 -0400

    Finally Edited

---
+++ BookController.java
@@ -0,0 +1,32 @@
+package Book;
+
+
+public class BookController {
+
+	Book b;
+	BookView view;
+	 
+	 BookController(Book b, BookView view){
+		 this.b = b;
+		 this.view = view;
+	 }
+	 public void setTitle(String title) {
+		 b.setTitle(title);
+	 }
+	 public void setPrice(double price) {
+		 b.setPrice(price);
+	 }
+	 public void setAuthor(Author a) {
+		 b.setMyAuthor(a);
+	 }
+	 public double getPrice() {
+		 return b.getPrice();
+	 }
+	 public String getTitle() {
+		 return  b.getTitle();
+	 }
+	 public Author getAuthor() {
+		 return b.getMyAuthor();
+	 }
+	 
+}
